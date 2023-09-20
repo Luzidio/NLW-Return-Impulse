50835cb505d1914baafd3502e21665135f86ac1c
Luzidio <luzidiorafaell@gmail.com>
2023-09-20 06:12:03 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257E5'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
